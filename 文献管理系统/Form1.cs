29d3c39d6dddea08486a0a3f7891ebaa3aa542f7
victorlw1 <53262655+victorlw1@users.noreply.github.com>
2020-03-28 19:10:48 +0800

    侧边菜单栏及图标设计
    wdnmd

--- 文献管理系统/Form1.cs
+++ 文献管理系统/Form1.cs
@@ -10,11 +10,60 @@ using System.Windows.Forms;
 
 namespace 文献管理系统
 {
-    public partial class Form1 : Form
+    public partial class 文献管理系统 : Form
     {
-        public Form1()
+        public 文献管理系统()
         {
             InitializeComponent();
+            customizeDesign();
+        }
+        private void customizeDesign()
+        {
+            子菜单panel.Visible = false;
+
+        }
+        private void hideSubMenu()
+        {
+            if(子菜单panel.Visible ==true)
+            {
+                子菜单panel.Visible = false;
+            }
+        }
+        private void showSubMenu(Panel subMenu)
+        {
+            if (子菜单panel.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 工具按钮_Click(object sender, EventArgs e)
+        {
+            showSubMenu(子菜单panel);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hideSubMenu();
         }
     }
 }
--- 文献管理系统/Program.cs
+++ 文献管理系统/Program.cs
@@ -16,7 +16,7 @@ namespace 文献管理系统
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new 文献管理系统());
         }
     }
 }
