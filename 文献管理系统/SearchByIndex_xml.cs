9994bab098f838640784177e11ba56a2fb02ed10
iamtomyum <574308565@qq.com>
2020-05-16 21:27:19 +0800

    应该没有bug

--- 文献管理系统/SearchByIndex_xml.cs
+++ 文献管理系统/SearchByIndex_xml.cs
@@ -28,13 +28,115 @@ namespace 文献管理系统
                 Infomation = new List<string>();
             }
 
-            while (reader.Read())
-            {
-                if (reader.NodeType == XmlNodeType.Element)
+            foreach (string index in indexGroup) {
+                while (reader.Read())
                 {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.GetAttribute("index") == index)
+                        {
+                            switch (reader.Name)
+                            {
+                                case "article":
+                                    reader.MoveToElement();//跳过空白节点 免得影响条件判断
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "author" || reader.Name == "volume" || reader.Name == "ee" || reader.Name == "isbn" || reader.Name == "url" || reader.Name == "journal" || reader.Name == "month" || reader.Name == "year" || reader.Name == "title" || reader.Name == "publisher"))
+                                        {
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
 
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+                                case "book":
+                                    reader.MoveToElement();
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "author" || reader.Name == "title" || reader.Name == "year" || reader.Name == "series" || reader.Name == "publisher" || reader.Name == "isbn" || reader.Name == "ee"))
+                                        {
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
+
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+                                case "incollection":
+                                    reader.MoveToElement();
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "author" || reader.Name == "title" || reader.Name == "pages" || reader.Name == "year" || reader.Name == "booktitle" || reader.Name == "url" || reader.Name == "crossref" || reader.Name == "ee" || reader.Name == "cdrom" || reader.Name == "note" || reader.Name == "month"))
+                                        {
+
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
+
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+                                case "inproceedings":
+                                    reader.MoveToElement();
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "author" || reader.Name == "title" || reader.Name == "pages" || reader.Name == "year" || reader.Name == "booktitle" || reader.Name == "url" || reader.Name == "crossref" || reader.Name == "ee" || reader.Name == "cdrom" || reader.Name == "note" || reader.Name == "month"))
+                                        {
+
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
+
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+                                case "www":
+                                    reader.MoveToElement();
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "volume" || reader.Name == "editor" || reader.Name == "author" || reader.Name == "title" || reader.Name == "pages" || reader.Name == "year" || reader.Name == "booktitle" || reader.Name == "url" || reader.Name == "crossref" || reader.Name == "ee" || reader.Name == "cdrom" || reader.Name == "note" || reader.Name == "month"))
+                                        {
+
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
+
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+
+                                case "phdthesis":
+                                    reader.MoveToElement();
+                                    while (reader.Read())
+                                    {
+                                        reader.MoveToContent();
+                                        if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "author" || reader.Name == "title" || reader.Name == "year" || reader.Name == "school" || reader.Name == "pages" || reader.Name == "publisher" || reader.Name == "series" || reader.Name == "volume" || reader.Name == "isbn" || reader.Name == "ee"))
+                                        {
+                                            this.Infomation.Add(reader.Name);
+                                            this.Infomation.Add(reader.ReadInnerXml());
+
+                                        }
+                                        else
+                                            break;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
                 }
             }
+            reader.Close();
 
 
             return Infomation;
